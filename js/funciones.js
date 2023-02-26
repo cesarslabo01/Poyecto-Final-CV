dc8efa68ad00ca9667bbf3f6e0cfa9bdada6a5ba
Cesar Slabosevicius <ce_gabriel02@hotmail.com>
2023-02-26 12:51:25 -0300

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,34 @@
+function ocultardatos3(){
+  var parrafo=document.getElementById("datosotrosest");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
+
+function ocultardatos(){
+  var parrafo=document.getElementById("datosexp");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
+function ocultardatos4(){
+  var parrafo=document.getElementById("datoscono");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
+
+function ocultardatos2(){
+  var parrafo=document.getElementById("datosest");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
